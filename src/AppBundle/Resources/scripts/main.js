16b6f2aba4e0035d9378fb1d18c493671dc8e640
Evaldas Pužauskas <evaldas.puz@gmail.com>
2017-12-09 14:09:27 +0200

    Added a new catalog

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -1,7 +1,7 @@
 /* Smooth scroll */
 
 $(function() {
-    $('a[href*="#"]:not([href="#adv_team_4_columns_carousel"])').click(function() {
+    $('a[href*="#"]:not([href="#myCarousel"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
