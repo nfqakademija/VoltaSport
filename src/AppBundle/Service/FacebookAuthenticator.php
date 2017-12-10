bbf1271acb722be6e7afd2abf12c4a33e393118c
Evaldas Pužauskas <evaldas.puz@gmail.com>
2017-12-10 10:45:41 +0200

    Travis fixes #6

--- src/AppBundle/Service/FacebookAuthenticator.php
+++ src/AppBundle/Service/FacebookAuthenticator.php
@@ -3,15 +3,16 @@
 namespace AppBundle\Service;
 
 use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
-use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
+use KnpU\OAuth2ClientBundle\Client\Provider\FacebookClient;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class FacebookAuthenticator extends SocialAuthenticator
 {
@@ -19,7 +20,7 @@ class FacebookAuthenticator extends SocialAuthenticator
     private $em;
     private $router;
 
-    public function __construct(ClientRegistry $clientRegistry, EntityManager $em, RouterInterface $router)
+    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $em, RouterInterface $router)
     {
         $this->clientRegistry = $clientRegistry;
         $this->em = $em;
@@ -31,28 +32,20 @@ class FacebookAuthenticator extends SocialAuthenticator
         if ($request->getPathInfo() != '/connect/facebook/check') {
             return;
         }
-        return $this->fetchAccessToken($this->getFacebookClient());
-    }
 
-    /**
-     * @return \KnpU\OAuth2ClientBundle\Client\OAuth2Client
-     */
-    private function getFacebookClient()
-    {
-        return $this->clientRegistry
-            ->getClient('facebook_main');
+        return $this->fetchAccessToken($this->getFacebookClient());
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         $client = $this->getFacebookClient();
+
+        $facebookUser = $client->fetchUserFromToken($credentials);
+
         $provider = $client->getOAuth2Provider();
         $token = $provider->getLongLivedAccessToken($credentials);
 
-        $facebookUser = $this->getFacebookClient()
-            ->fetchUserFromToken($credentials);
-
-        $existingUser = $this->em->getRepository('AppBundle:User')
+        $existingUser = $this->em->getRepository(User::class)
             ->findOneBy(['facebookId' => $facebookUser->getId()]);
 
         if ($existingUser) {
@@ -60,6 +53,7 @@ class FacebookAuthenticator extends SocialAuthenticator
         }
 
         $user = new User();
+
         $user->setEmail($facebookUser->getEmail());
         $user->setName($facebookUser->getName());
         $user->setUsername($facebookUser->getName());
@@ -70,6 +64,15 @@ class FacebookAuthenticator extends SocialAuthenticator
         $this->em->flush();
 
         return $user;
+    }
+
+    /**
+     * @return FacebookClient
+     */
+    private function getFacebookClient()
+    {
+        return $this->clientRegistry
+            ->getClient('facebook_main');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
@@ -84,8 +87,6 @@ class FacebookAuthenticator extends SocialAuthenticator
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        return new RedirectResponse(
-            '/'
-        );
+        return new RedirectResponse('/');
     }
 }
